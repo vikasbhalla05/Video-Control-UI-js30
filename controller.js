f92c7c6c0cb7d96d5595d6b247319b8a7f47d971
vikasbhalla05 <77003706+vikasbhalla05@users.noreply.github.com>
2022-05-26 09:00:10 +0530

    Applied my own logical understanding

---
+++ controller.js
@@ -0,0 +1,31 @@
+let video = document.querySelector(".wrapper > video");
+let control = document.querySelector(".speed");
+let speed = document.querySelector(".speed > .speed-bar");
+let isDown = false;
+
+
+
+control.addEventListener('mousedown', (e) => {
+	isDown = true;
+})
+
+control.addEventListener('mouseup', (e) => {
+	isDown = false
+})
+control.addEventListener('mouseleave', (e) => {
+	isDown = false;
+})
+
+control.addEventListener('mousemove', (e) => {
+	if(isDown){
+		let speedHeight = e.pageY-control.offsetTop;
+		speed.style.height = `${speedHeight}px`;
+		let videoRate = (control.offsetHeight - control.offsetTop) * speedHeight/10000;
+		video.playbackRate = videoRate;
+		speed.textContent = `${Math.floor(videoRate)}x`;
+		console.log(videoRate);
+	}
+	else{
+		return;
+	}
+})
